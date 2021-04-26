6c465ddc42a65340cdfe5a1fd5e36c26b1ebf415
Arijit Saha <arijitbiley@gmail.com>
2021-04-26 20:12:27 +0530

    Add welcome message in home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: FC<HeaderProps> = ({ activePage }) => {
   return (
     <>
       <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-primary mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full sm:w-10/12 container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
             <Link
               to="/"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,76 +1,26 @@
 import React from "react";
 
 import Layout from "../components/Layout";
+import ME_IMG from "../../static/img/Arijit Saha.jpg";
+import "./index.css";
 
 const IndexPage = () => {
   return (
     <Layout>
-      <div className="grid grid-cols-1 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">
-        <div className="relative z-10 col-start-1 row-start-1 px-4 pt-40 pb-3 bg-gradient-to-t from-black sm:bg-none">
-          <p className="text-sm font-medium text-white sm:mb-1 sm:text-gray-500">
+      <div className="w-4/5 mx-auto grid grid-cols-1 min-h-full auto-rows-max sm:grid-cols-4 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16">
+        <div className="z-10 row-span-full col-end-4 col-span-3 px-4 pt-40 pb-3 bg-gradient-to-t from-black sm:bg-none">
+          {/*<p className="text-sm font-medium text-white sm:mb-1 sm:text-gray-500">
             Entire house
-          </p>
-          <h2 className="text-xl font-semibold text-white sm:text-2xl sm:leading-7 sm:text-black md:text-3xl">
-            Beach House in Collingwood
+          </p>*/}
+          <h2 className="text-xl font-semibold text-white sm:text-2xl sm:leading-10 md:text-5xl">
+            I am a CS undergrad who loves to solve problems and learn new
+            technologies
           </h2>
         </div>
-        <div className="col-start-1 row-start-2 px-4 sm:pb-16">
-          <div className="flex items-center text-sm font-medium my-5 sm:mt-2 sm:mb-4">
-            <svg
-              width="20"
-              height="20"
-              fill="currentColor"
-              className="text-violet-600"
-            >
-              <path d="M9.05 3.691c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.372 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.539 1.118l-2.8-2.034a1 1 0 00-1.176 0l-2.8 2.034c-.783.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.363-1.118l-2.8-2.034c-.784-.57-.381-1.81.587-1.81H7.03a1 1 0 00.95-.69L9.05 3.69z" />
-            </svg>
-            <div className="ml-1">
-              <span className="text-black">4.94</span>
-              <span className="sm:hidden md:inline">(128)</span>
-            </div>
-            <div className="text-base font-normal mx-2">·</div>
-            <div>Collingwood, Ontario</div>
-          </div>
-          <hr className="w-16 border-gray-300 hidden sm:block" />
-        </div>
-        <div className="col-start-1 row-start-3 space-y-3 px-4">
-          <p className="flex items-center text-black text-sm font-medium">
-            <img
-              src="/kevin-francis.jpg"
-              alt=""
-              className="w-6 h-6 rounded-full mr-2 bg-gray-100"
-            />
-            Hosted by Kevin Francis
-          </p>
-          <button
-            type="button"
-            className="bg-violet-100 text-violet-700 text-base font-semibold px-6 py-2 rounded-lg"
-          >
-            Check availability
-          </button>
-        </div>
-        <div className="col-start-1 row-start-1 flex sm:col-start-2 sm:row-span-3">
-          <div className="w-full grid grid-cols-3 grid-rows-2 gap-2">
-            <div className="relative col-span-3 row-span-2 md:col-span-2">
-              <img
-                src="/beach-house.jpg"
-                alt=""
-                className="absolute inset-0 w-full h-full object-cover bg-gray-100 sm:rounded-lg"
-              />
-            </div>
-            <div className="relative hidden md:block">
-              <img
-                src="/beach-house-interior.jpg"
-                alt=""
-                className="absolute inset-0 w-full h-full object-cover rounded-lg bg-gray-100"
-              />
-            </div>
-            <div className="relative hidden md:block">
-              <img
-                src="/beach-house-view.jpg"
-                alt=""
-                className="absolute inset-0 w-full h-full object-cover rounded-lg bg-gray-100"
-              />
+        <div className="row-span-full col-start-1 flex sm:col-span-2 sm:col-end-5">
+          <div className="w-full grid grid-cols-1 gap-2">
+            <div className="col-span-1 md:col-span-1">
+              <img src={ME_IMG} alt="" className="inset-0 w-full bg-gray-100" />
             </div>
           </div>
         </div>
@@ -79,20 +29,6 @@ const IndexPage = () => {
   );
 };
 
-//   <div className="topnav">
-//       <div className="brand">
-//         <a className="name" href="index.html">ARIJIT SAHA</a>
-//       </div>
-//       <div className="navlinks">
-//         <!-- <div>
-//           <a href="index.html" className="active">HOME</a>
-//         </div>
-//         <div>
-//           <a href="projects.html">PROJECTS</a>
-//         </div> -->
-//       </div>
-//     </div>
-//
 //     <div className="introDiv">
 //       <div className="mediaLinks">
 //         <a
