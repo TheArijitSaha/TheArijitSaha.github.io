a40234b10df56b97c9a5c622ad9f7c0c0da6299d
Arijit Saha <arijitbiley@gmail.com>
2021-05-14 20:11:18 +0530

    Shift data to data folder

--- src/components/ProjectsPane/index.tsx
+++ src/components/ProjectsPane/index.tsx
@@ -1,62 +1,7 @@
 import React from "react";
 
-import Project, { ProjectProps } from "./Project";
-import mymdbImg from "../../../static/img/projects/MyMDbImg.png";
-import graphTheoreticTextAnalysisImg from "../../../static/img/projects/graphTextImg.png";
-import iiestNexusImg from "../../../static/img/projects/IIESTNexusImg.png";
-import crioStudentDeveloperImg from "../../../static/img/projects/crioCertImg.png";
-
-const projects: ProjectProps[] = [
-  {
-    title: "Student Backend Developer",
-    bgImg: crioStudentDeveloperImg,
-    desc:
-      "Developed three independent projects involving linux, web development and data structures and algorithms. Check out the portfolio for specifics.",
-    refs: [
-      {
-        text: "Portfolio Link",
-        link: "https://criodo.github.io/Crio-Launch-Feb-2020-arijitbiley/",
-      },
-    ],
-  },
-  {
-    title: "MyMDb",
-    bgImg: mymdbImg,
-    desc:
-      "A web app to keep track of what movies, series you have watched or wish to watch.",
-    refs: [
-      {
-        text: "Repository Link",
-        link: "https://github.com/TheArijitSaha/MyMDb",
-      },
-    ],
-  },
-  {
-    title: "Graph Theoretic Text Analysis",
-    bgImg: graphTheoreticTextAnalysisImg,
-    desc:
-      "Seeking out representative keywords from text by constructing co-occurence graphs.",
-    refs: [
-      {
-        text: "Repository Link",
-        link: "https://github.com/TheArijitSaha/Graph_Theoretic_Text_Analysis",
-      },
-    ],
-  },
-  {
-    title: "Skill-based social network",
-    bgImg: iiestNexusImg,
-    desc:
-      "A social networking web app with chat, post and search facility. Users can find people based on skills. Followers can view posts, like and comment.",
-    refs: [
-      {
-        text: "Repository Link",
-        link:
-          "https://github.com/TheArijitSaha/Web-Tech-5thSem-Assignments/tree/master/IIEST_Nexus",
-      },
-    ],
-  },
-];
+import Project from "./Project";
+import projects from "../../data/projects";
 
 const ProjectsPane = () => {
   return (
---
+++ src/data/experiences.ts
@@ -0,0 +1,47 @@
+import { ExperienceProps } from "../components/WorkPane/Experience";
+import isiLogoImg from "../../../static/img/experiences/ISILogoImg.png";
+import googleLogoImg from "../../../static/img/experiences/GoogleLogoImg.jpg";
+import msLogoImg from "../../../static/img/experiences/MSLogoImg.jpg";
+
+const experiences: ExperienceProps[] = [
+  {
+    title: "Google SPS",
+    bgImg: googleLogoImg,
+    period: "Summer 2020",
+    desc: "Developed a website to create, outsource and accept tasks",
+    refs: [
+      {
+        text: "Certificate",
+        link: "https://drive.google.com/file/d/1RQ2l48sf8wsuQVqNxkbHr0vjf4OCy8m9/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    title: "Microsoft",
+    subtitle: "Software Developer Intern",
+    period: "Summer 2020",
+    bgImg: msLogoImg,
+    desc: "As part of the Edge PDF team, enhanced user experience by adding an annotation feature",
+    refs: [
+      {
+        text: "Certificate",
+        link: "https://drive.google.com/file/d/1P31Xq8noXwuDG_7WAjCRh9vX_QsAT2_Y/view?usp=sharing",
+      },
+    ],
+  },
+  {
+    title: "ISI Kolkata",
+    subtitle: "Research Intern",
+    bgImg: isiLogoImg,
+    period: "Summer 2019",
+    desc: "Worked on Quantum Cryptographic simulations using IBM's Quantum Computer and QUISKIT",
+    refs: [
+      {
+        text: "Certificate",
+        link: "https://drive.google.com/file/d/1ODYt3J0qx4jnjmZwrhxzwVkTYyqA1ckT/view?usp=sharing",
+      },
+    ],
+  },
+];
+
+export default experiences;
