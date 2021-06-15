6103b2d1544f17851c7bd8a940697f217f749cd6
Arijit Saha <arijitbiley@gmail.com>
2021-06-15 14:08:32 +0530

    Add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,46 @@
 import { ProjectProps } from "../components/ProjectsPane/Project";
+import haven from "../../static/img/projects/Haven.png";
+import consoleTetris from "../../static/img/projects/ConsoleTetris.png";
 import mymdbImg from "../../static/img/projects/MyMDbImg.png";
+import mymdbApp from "../../static/img/projects/MyMDbApp.png";
 import graphTheoreticTextAnalysisImg from "../../static/img/projects/graphTextImg.png";
 import iiestNexusImg from "../../static/img/projects/IIESTNexusImg.png";
 import crioStudentDeveloperImg from "../../static/img/projects/crioCertImg.png";
 
 const projects: ProjectProps[] = [
+  {
+    title: "Haven",
+    bgImg: haven,
+    desc: "A react native based mobile crowdsensing app to track COVID-appropriate behaviour at places of interest.",
+    refs: [
+      {
+        text: "Repository Link",
+        link: "https://github.com/TheArijitSaha/Haven",
+      },
+    ],
+  },
+  {
+    title: "MyMDb Native App",
+    bgImg: mymdbApp,
+    desc: "A react native app to keep track of what movies, series I have watched or wish to watch.",
+    refs: [
+      {
+        text: "Repository Link",
+        link: "https://github.com/TheArijitSaha/MyMDb-RN-App",
+      },
+    ],
+  },
+  {
+    title: "Console Tetris",
+    bgImg: consoleTetris,
+    desc: "A c++ app to play tetris in the linux console.",
+    refs: [
+      {
+        text: "Repository Link",
+        link: "https://github.com/TheArijitSaha/console-tetris",
+      },
+    ],
+  },
   {
     title: "Student Backend Developer",
     bgImg: crioStudentDeveloperImg,
@@ -17,13 +53,13 @@ const projects: ProjectProps[] = [
     ],
   },
   {
-    title: "MyMDb",
+    title: "MyMDb Web App",
     bgImg: mymdbImg,
-    desc: "A web app to keep track of what movies, series you have watched or wish to watch.",
+    desc: "A PHP web app to keep track of what movies, series you have watched or wish to watch.",
     refs: [
       {
         text: "Repository Link",
-        link: "https://github.com/TheArijitSaha/MyMDb",
+        link: "https://github.com/TheArijitSaha/MyMDb-Web",
       },
     ],
   },
